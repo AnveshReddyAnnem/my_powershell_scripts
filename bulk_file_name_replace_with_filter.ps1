62109e201a491c5adc31d805a0e3bd2cd107d7f5
AnveshReddyAnnem <57712430+AnveshReddyAnnem@users.noreply.github.com>
2022-03-31 15:31:27 +0530

    Add files via upload

---
+++ bulk_file_name_replace_with_filter.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Bulk File name replace one liner for my custom requirements, ***Anvesh Reddy Annem***.`n"
+
+$source      = Read-Host "`nEnter the folder in which files names are to be replaced"
+$matchFilter = Read-Host "`nEnter the string to filter files, else leave it empty"
+$oldName     = Read-Host "`nString in Old file name"
+$newName     = Read-Host "`nString in New file name"
+
+
+Get-ChildItem -Path $source | Where-Object { $_.Name -cmatch 'i_' } | Rename-Item -NewName {$_.Name -replace $oldName,$newName}
